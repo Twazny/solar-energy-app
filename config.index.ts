079a444a9b02d8ee1a3f491b9258adec4389102c
Tomasz Wazny <wazny.tomek@gmail.com>
2021-02-08 11:37:45 +0100

    workflow test 2

--- config.index.ts
+++ config.index.ts
@@ -7,7 +7,7 @@ const envConfigFile = `export const environment = {
    climacellAPIKey: '${process.env.CLIMACELL_API_KEY}'
 };
 `;
-console.log(process.env)
+console.log(process.env.CLIMACELL_API_KEY)
 writeFile(targetPath, envConfigFile, 'utf8', (err) => {
     if (err) {
         return console.log(err);
